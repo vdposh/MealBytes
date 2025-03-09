65c9606e7bc627991b2c25a1076f8dc57f47c37b
Vlad Posherstnik <iamporshe@gmail.com>
2025-03-09 19:41:11 +0300

    corrected FoodVM, SearchVM, Structs

---
+++ MealBytes/View/FoodView/FoodViewModel/NutrientBlock/NutrientBlock.swift
@@ -0,0 +1,14 @@
+//
+//  NutrientBlock.swift
+//  MealBytes
+//
+//  Created by Porshe on 09/03/2025.
+//
+
+import SwiftUI
+
+struct NutrientBlock {
+    let title: String
+    let value: Double
+    let unit: String
+}
---
+++ MealBytes/View/FoodView/FoodViewModel/NutrientDetail/NutrientDetail.swift
@@ -0,0 +1,15 @@
+//
+//  NutrientDetail.swift
+//  MealBytes
+//
+//  Created by Porshe on 09/03/2025.
+//
+
+import SwiftUI
+
+struct NutrientDetail {
+    let title: String
+    let value: Double
+    let unit: String
+    let isSubValue: Bool
+}
