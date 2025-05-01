6b5bc46bb5a1d8c191a7f4842b02258afa24278a
Vlad Posherstnik <iamporshe@gmail.com>
2025-05-01 10:10:59 +0300

    revert

--- MealBytes/View/Diary/MainView/MainViewModel/MainViewModel.swift
+++ MealBytes/View/Diary/MainView/MainViewModel/MainViewModel.swift
@@ -20,6 +20,7 @@ final class MainViewModel: ObservableObject {
     @Published var mealItems: [MealType: [MealItem]]
     @Published var nutrientSummaries: [NutrientType: Double]
     @Published var expandedSections: [MealType: Bool] = [:]
+    @Published var uniqueID = UUID()
     @Published var appError: AppError?
     @Published var rdiProgress: Double = 0.0
     @Published var rdi: String = ""
