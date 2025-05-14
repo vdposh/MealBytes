fb7a11ecf0afebe1cb18293c66aef381fe33d55d
vdposh <iamporshe@gmail.com>
2025-05-14 11:46:27 +0300

    added Preview

--- MealBytes/View/Diary/FoodView/FoodViewModel/FoodViewModel.swift
+++ MealBytes/View/Diary/FoodView/FoodViewModel/FoodViewModel.swift
@@ -327,23 +327,23 @@ enum MeasurementUnit: String, CaseIterable, Identifiable {
     var id: String { self.rawValue }
 }
 
-//#Preview {
-//    FoodView(
-//        isDismissedFoodView: .constant(true),
-//        navigationTitle: "Add to Diary",
-//        food: Food(
-//            searchFoodId: 3092,
-//            searchFoodName: "Egg",
-//            searchFoodDescription: "1 cup"
-//        ),
-//        searchViewModel: SearchViewModel(mainViewModel: MainViewModel()),
-//        mainViewModel: MainViewModel(),
-//        mealType: .breakfast,
-//        amount: "1",
-//        measurementDescription: "Grams",
-//        showAddButton: false,
-//        showSaveRemoveButton: true,
-//        showMealTypeButton: true,
-//        originalMealItemId: UUID()
-//    )
-//}
+#Preview {
+    FoodView(
+        isDismissed: .constant(true),
+        navigationTitle: "Add to Diary",
+        food: Food(
+            searchFoodId: 3092,
+            searchFoodName: "Egg",
+            searchFoodDescription: "1 cup"
+        ),
+        searchViewModel: SearchViewModel(mainViewModel: MainViewModel()),
+        mainViewModel: MainViewModel(),
+        mealType: .breakfast,
+        amount: "1",
+        measurementDescription: "Grams",
+        showAddButton: false,
+        showSaveRemoveButton: true,
+        showMealTypeButton: true,
+        originalMealItemId: UUID()
+    )
+}
